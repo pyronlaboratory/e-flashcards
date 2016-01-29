55e7d7694c7b7997b846d4047980b485bfe2cd3d
Ronit Mishra <ronnie94official@gmail.com>
2016-01-29 12:42:10 +0530

    QuizCard.java

---
+++ QuizCard.java
@@ -0,0 +1,18 @@
+package com.persistent.training.serialization;
+
+public class QuizCard {
+	
+	
+	String question;
+	String answer;
+	public QuizCard(Object question, Object answer) {
+		
+	}
+	public String getQuestion() {
+		return question;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+
+}
